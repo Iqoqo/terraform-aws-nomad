6c2a0188eb2538c211d0a7d4d713d904babdc415
yoadfe <yoad.fekete@iqoqo.co>
2020-04-27 17:25:24 +0300

    fix params no

--- modules/install-nomad/install-nomad-disco.sh
+++ modules/install-nomad/install-nomad-disco.sh
@@ -163,8 +163,8 @@ function create_nomad_install_paths {
 function install_binaries {
   local readonly environment="$1"
   local readonly version="$2"
-  local readonly path="$2"
-  local readonly username="$3"
+  local readonly path="$3"
+  local readonly username="$4"
   local url="https://s3-us-west-2.amazonaws.com/disco.nomad.$environment/latest/nomad_latest_linux_amd64"
   if [[ "${var.environment}" == "production" ]]; then
     url="https://s3-us-west-2.amazonaws.com/disco.nomad/latest/nomad_latest_linux_amd64"
